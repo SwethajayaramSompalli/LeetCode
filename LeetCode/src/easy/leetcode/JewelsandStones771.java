c93a7920b69d6f41265527287ce5a5f126d5f47b
Swetha <49846067+SwethajayaramSompalli@users.noreply.github.com>
2020-12-08 13:46:32 -0600

    JewelsandStones771 - not complete
    Will come back after I read collections

---
+++ LeetCode/src/easy/leetcode/JewelsandStones771.java
@@ -0,0 +1,28 @@
+package easy.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JewelsandStones771 {
+
+}
+
+class JewelsandStones771Solution {
+    public int numJewelsInStones(String J, String S) {
+        if(J==null ||S==null) return 0;
+        int sum =0;
+        Map<Integer, Character> map = new HashMap<>();
+        for(int i=0; i<J.length();i++){
+            map.put(i,J.charAt(i));
+            
+        }
+        for (int i =0; i<S.length();i++){
+            if()
+            
+        }
+        
+        
+        
+        //1 loop and 1 hashmap
+    }
+}
