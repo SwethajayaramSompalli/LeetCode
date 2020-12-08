be53b4673672ea7eaed99a1c9f8db8138279db27
Swetha <49846067+SwethajayaramSompalli@users.noreply.github.com>
2020-12-07 19:51:03 -0600

    RunningSumOf1dArray1480

--- LeetCode/src/easy/leetcode/RunningSumOf1dArray1480.java
+++ LeetCode/src/easy/leetcode/RunningSumOf1dArray1480.java
@@ -11,3 +11,6 @@ class RunningSumOf1dArray1480Solution {
         return nums;
     }
 }
+
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Running Sum of 1d Array.
+//Memory Usage: 40 MB, less than 5.52% of Java online submissions for Running Sum of 1d Array.
