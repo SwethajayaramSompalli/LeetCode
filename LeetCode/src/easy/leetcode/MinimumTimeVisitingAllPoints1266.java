5192a751a1cf2c5a5e511141d63679a9c622efd5
Swetha <49846067+SwethajayaramSompalli@users.noreply.github.com>
2021-01-03 00:21:21 -0600

    MinimumTimeVisitingAllPoints1266Solution

--- LeetCode/src/easy/leetcode/MinimumTimeVisitingAllPoints1266.java
+++ LeetCode/src/easy/leetcode/MinimumTimeVisitingAllPoints1266.java
@@ -5,14 +5,14 @@ public class MinimumTimeVisitingAllPoints1266 {
 }
 
 class MinimumTimeVisitingAllPoints1266Solution {
-    public int minTimeToVisitAllPoints(int[][] points) {
+	public int minTimeToVisitAllPoints(int[][] points) {
         int steps=0;
         for(int i =0; i< points.length-1; i++){
-            int x = Math.abs(points[i][0]- points[i+1][0]);
-            int y = Math.abs(points[i][1]- points[i+1][1]);
-            int d = Math.abs(x-y);
-            int c = Math.abs(Math.max(x,y)-d);
-            steps+= d+c;
+            int x = Math.abs(points[i][0]- points[i+1][0]); //find the x axis difference for two points
+            int y = Math.abs(points[i][1]- points[i+1][1]); //find the y axis difference for two points
+            int d = Math.abs(x-y); //Now find equal no of steps in x and y that we can take diagonally
+            int c = Math.abs(Math.max(x,y)-d); //Find the extra steps that need to be taken either vertically or horizontally
+            steps+= d+c; // add both common and difference steps.
         }
         return steps;
     }
